974d7e5822df5708731cf402c7eecfdd15356ef7
pale102 <mcube312@gmail.com>
2026-07-15 07:18:55 +0100

    app loaders

---
+++ client/src/components/Loader.jsx
@@ -0,0 +1,7 @@
+const Loader = () => (
+  <div className="flex justify-center py-12">
+    <div className="w-8 h-8 border-2 border-border border-t-accent rounded-full animate-spin" />
+  </div>
+);
+
+export default Loader;
