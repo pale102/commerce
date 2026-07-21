19cd98fcaa816d2a629cac57d73c4d5452b4b0a9
pale102 <mcube312@gmail.com>
2026-07-21 15:54:23 +0100

    routes

---
+++ client/src/routes/ProtectedRoute.jsx
@@ -0,0 +1,14 @@
+import { useSelector } from "react-redux";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+
+const ProtectedRoute = () => {
+  const { user } = useSelector((state) => state.auth);
+  const location = useLocation();
+
+  if (!user) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+  return <Outlet />;
+};
+
+export default ProtectedRoute;
